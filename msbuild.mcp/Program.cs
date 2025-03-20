02b9b6f71268ec7eea2f7ac3355348b2c8183fe5
Chet Husk <chusk3@gmail.com>
2025-03-20 15:19:01 -0500

    prompty prompt prompt

--- msbuild.mcp/Program.cs
+++ msbuild.mcp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using McpDotNet;
+using McpDotNet.Protocol.Types;
 using McpDotNet.Server;
 using Microsoft.Build.Evaluation;
 using Microsoft.Build.Locator;
@@ -7,6 +8,108 @@ using Microsoft.Extensions.Hosting;
 using Serilog;
 
 namespace MSBuild.MCP;
+
+public static class Prompts
+{
+    public class PromptArgument<T> : PromptArgument
+    {
+        public PromptArgument() : base() { }
+    }
+
+    public class TextContent : Content
+    {
+        public TextContent(string message) : base()
+        {
+            Type = "text";
+            Text = message;
+        }
+    }
+    public class ImageContent : Content
+    {
+        public ImageContent(string data, string mimeType) : base()
+        {
+            Type = "image";
+            Data = data;
+            MimeType = mimeType;
+        }
+    }
+    public class EmbeddedResource : Content
+    {
+        public EmbeddedResource(ResourceContents resource) : base()
+        {
+            Type = "resource";
+            Resource = resource;
+        }
+    }
+
+    public static Prompt UpgradeProjectPrompt = new()
+    {
+        Name = "upgrade-project",
+        Description = "Upgrade a project to the given target framework",
+        Arguments = new() {
+            new PromptArgument<string>(){
+                Name = "projectPath",
+                Description = "The path to the project file to upgrade",
+                Required = true
+            },
+            new PromptArgument<string>() {
+                Name = "targetFramework",
+                Description = "The target framework to upgrade to",
+                Required = true
+            }
+        }
+    };
+
+    public static Prompt UpgradeAllProjectsPrompt = new()
+    {
+        Name = "upgrade-all-project",
+        Description = "Upgrade a project to the given target framework",
+        Arguments = new() {
+            new PromptArgument<string[]>(){
+                Name = "projects",
+                Description = "The paths to the projects to upgrade.",
+                Required = true
+            },
+            new PromptArgument<string>() {
+                Name = "targetFramework",
+                Description = "The target framework to upgrade to.",
+                Required = true
+            }
+        }
+    };
+
+    public static List<Prompt> All = [UpgradeProjectPrompt, UpgradeAllProjectsPrompt];
+
+    public static GetPromptResult GetPrompt(GetPromptRequestParams req)
+    {
+        return req.Name switch
+        {
+            "upgrade-project" => new GetPromptResult()
+            {
+                Description = "Upgrade a project to the given target framework",
+                Messages = new() {
+                    new (){ Role = Role.User, Content = new TextContent($"Update the project at {req.Arguments!["projectPath"]} to target framework {req.Arguments!["targetFramework"]}")},
+                 }
+            },
+            "upgrade-all-projects" => new GetPromptResult()
+            {
+                Description = "Upgrade the selected projects to the given target framework",
+                Messages = new() {
+                    new (){ Role = Role.User, Content = new TextContent(
+                        $"""
+                        Update the projects at {string.Join(", ", req.Arguments!["projects"])} to target framework {req.Arguments!["targetFramework"]}.
+                        Make me a plan to update the target frameworks for these projects.
+                        The plan should update the projects in dependency order - projects with no project dependencies should be first, then projects whose project dependencies have been upgraded, and so on.
+                        Determine the dependency order by using the list-project-dependencies tool on each project.
+                        Also output this plan to a file named `plan.md`.
+                        """
+                    ) },
+                 }
+            },
+            _ => new GetPromptResult()
+        };
+    }
+}
 
 [McpToolType]
 public class MSBuildTool
@@ -33,6 +136,15 @@ public class MSBuildTool
         var project = TryLoadProject(projectPath);
         return project.GetItems("ProjectReference").Select(i => i.EvaluatedInclude).ToArray();
     }
+
+    [McpTool("list-package-references"), Description("Returns the package references of a project")]
+    /// <param name="projectPath">The path to the project file to read</param>
+    public string[] ListPackageReferences(string projectPath)
+    {
+        var project = TryLoadProject(projectPath);
+        return project.GetItems("PackageReference").Select(i => i.EvaluatedInclude).ToArray();
+    }
+
 
     Project TryLoadProject(string projectPath)
     {
@@ -73,7 +185,9 @@ public static class Program
         builder.Services
             .AddMcpServer()
             .WithStdioServerTransport()
-            .WithTools();
+            .WithTools()
+            .WithListPromptsHandler((ctx, ctok) => Task.FromResult(new ListPromptsResult() { Prompts = Prompts.All }))
+            .WithGetPromptHandler((req, ctok) => Task.FromResult(Prompts.GetPrompt(req.Params!)));
         await builder.Build().RunAsync();
     }
 }
